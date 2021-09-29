c6a2984dac70d5624be2153f01ddd2aec50c7a17
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-09-29 17:33:55 -0300

    feat: /domain/usecases Authentication

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,13 @@
+import { AccountModel } from '@/domain/models'
+
+export interface Authentication {
+  auth: (credentials: Authentication.Params) => Promise<Authentication.Result>
+}
+
+export namespace Authentication {
+  export type Params = {
+    email: string
+    password: string
+  }
+  export type Result = AccountModel
+}
