862522515fcb582bf94d08f3498a7ce1ea850756
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-09-30 11:25:03 -0300

    refactor: /data/protocols HttpPostClient change params received

--- tests/data/mocks/mock-http-post-client.ts
+++ tests/data/mocks/mock-http-post-client.ts
@@ -2,8 +2,11 @@ import { HttpPostClient } from '@/data/protocols'
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
-  async post (url: string): Promise<void> {
-    this.url = url
+  body?: any
+
+  async post (params: HttpPostClient.Params): Promise<void> {
+    this.url = params.url
+    this.body = params.body
     return Promise.resolve()
   }
 }
