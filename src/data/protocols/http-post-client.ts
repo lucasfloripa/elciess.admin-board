2cb06e259e2f961264a84454e4f0e1f8ea16bd64
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-10-02 16:50:29 -0300

    refactor: add httpPostClient Result values

--- src/data/protocols/http-post-client.ts
+++ src/data/protocols/http-post-client.ts
@@ -1,10 +1,14 @@
 export interface HttpPostClient {
-  post: (params: HttpPostClient.Params) => Promise<void>
+  post: (params: HttpPostClient.Params) => Promise<HttpPostClient.Result>
 }
 
 export namespace HttpPostClient {
   export type Params = {
     url: string
+    body?: any
+  }
+  export type Result = {
+    statusCode: number
     body?: any
   }
 }
--- tests/data/mocks/mock-http-post-client.ts
+++ tests/data/mocks/mock-http-post-client.ts
@@ -3,10 +3,13 @@ import { HttpPostClient } from '@/data/protocols'
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
   body?: any
+  response: HttpPostClient.Result = {
+    statusCode: 200
+  }
 
-  async post (params: HttpPostClient.Params): Promise<void> {
+  async post (params: HttpPostClient.Params): Promise<HttpPostClient.Result> {
     this.url = params.url
     this.body = params.body
-    return Promise.resolve()
+    return Promise.resolve(this.response)
   }
 }
