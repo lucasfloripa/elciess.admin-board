b98f608df86a7c8f6256ccafe981df791e3bba86
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-10-24 11:16:06 -0300

    feat: ensure RemoteAuthentication return an Account if httpPostClient returns 200

--- src/data/usecases/remote-authentication.ts
+++ src/data/usecases/remote-authentication.ts
@@ -2,24 +2,26 @@ import { Authentication } from '@/domain/usecases'
 import { HttpPostClient } from '@/data/protocols'
 import { InvalidCredentialsError, UnexpectedError, ServerError } from '@/domain/errors'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor (
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<Authentication.Params, Authentication.Result>
   ) { }
 
-  async auth (credentials: Authentication.Params): Promise<void> {
-    const httpReponse = await this.httpPostClient.post({
+  async auth (credentials: Authentication.Params): Promise<Authentication.Result> {
+    const httpPostResponse = await this.httpPostClient.post({
       url: this.url,
       body: credentials
     })
-    switch (httpReponse.statusCode) {
+    switch (httpPostResponse.statusCode) {
       case 400:
         throw new UnexpectedError()
       case 401:
         throw new InvalidCredentialsError()
       case 500:
         throw new ServerError()
+      case 200:
+        return httpPostResponse.body
     }
   }
 }
