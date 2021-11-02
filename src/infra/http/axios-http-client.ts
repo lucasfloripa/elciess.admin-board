1bed8442050e60b24f852917f84093302ab5f77b
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-11-02 20:55:16 -0300

    feat: ensure AxiosHttpClient call post with correct url, body and verb

--- src/infra/http/axios-http-client.ts
+++ src/infra/http/axios-http-client.ts
@@ -2,8 +2,8 @@ import { HttpPostClient } from '@/data/protocols'
 
 import axios from 'axios'
 
-export class AxiosHttpClient {
-  async post (params: HttpPostClient.Params<any>): Promise<void> {
-    await axios.post(params.url)
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+  async post (params: HttpPostClient.Params<any>): Promise<HttpPostClient.Result<any>> {
+    return await axios.post(params.url, params.body)
   }
 }
