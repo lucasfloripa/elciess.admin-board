50ddee8e09ba88f6c6b27dfab5ebf53f088208f7
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-11-08 08:28:02 -0300

    feat: /presentation/pages/login Login

---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,11 @@
+import { LoginHeader } from '@/presentation/components/login'
+
+import React from 'react'
+
+const Login: React.FC = () => {
+  return (
+    <LoginHeader />
+  )
+}
+
+export { Login }
