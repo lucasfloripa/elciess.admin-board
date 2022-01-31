5621d05ce11d76994f455463f39a25e2f71c5d7e
Lucas Gonçalves <lucas.floripa@icloud.com>
2022-01-31 14:46:45 -0300

    test: should throw UnexpectError if HttpPostClient returns a non-register statusCode

--- tests/data/usescases/remote-authentication.spec.ts
+++ tests/data/usescases/remote-authentication.spec.ts
@@ -26,6 +26,16 @@ describe('/data/usecases RemoteAuthentication', () => {
     expect(httpPostClientSpy.url).toBe(url)
     expect(httpPostClientSpy.body).toBe(credentials)
   })
+  test('Should return an AccountModel if HttpPostClient returns 200', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const httpPostResult = mockAccountModel()
+    httpPostClientSpy.response = {
+      statusCode: 200,
+      body: httpPostResult
+    }
+    const account = await sut.auth(mockAuthenticationCredencials())
+    expect(account).toEqual(httpPostResult)
+  })
   test('Should throw UnexpectError if HttpPostClient returns 400', async () => {
     const { sut, httpPostClientSpy } = makeSut()
     httpPostClientSpy.response = {
@@ -50,14 +60,12 @@ describe('/data/usecases RemoteAuthentication', () => {
     const promise = sut.auth(mockAuthenticationCredencials())
     await expect(promise).rejects.toThrow(new ServerError())
   })
-  test('Should return an AccountModel if HttpPostClient returns 200', async () => {
+  test('Should throw UnexpectError if HttpPostClient returns a non-register statusCode', async () => {
     const { sut, httpPostClientSpy } = makeSut()
-    const httpPostResult = mockAccountModel()
     httpPostClientSpy.response = {
-      statusCode: 200,
-      body: httpPostResult
+      statusCode: 111
     }
-    const account = await sut.auth(mockAuthenticationCredencials())
-    expect(account).toEqual(httpPostResult)
+    const promise = sut.auth(mockAuthenticationCredencials())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
