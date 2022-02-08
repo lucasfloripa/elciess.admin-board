ca8b430768e84de9ddd127919fc86d8f361b632f
Lucas Gonçalves <lucas.floripa@icloud.com>
2022-02-08 14:20:53 -0300

    refactor: adapte jest to tests tsx files

---
+++ tests/presentation/pages/login.spec.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Login } from '@/presentation/pages'
+
+import { render } from '@testing-library/react'
+
+describe('Login Page', () => {
+  test('', async () => {
+    render(<Login />)
+  })
+})
